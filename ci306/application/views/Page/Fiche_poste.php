1bb8de854a29f4620cb68fb9f9961194defd883f
Hery00 <handrianimanana@gmail.com>
2023-03-19 00:11:38 +0300

    format fiche de poste mbola maratsa kely

--- ci306/application/views/Page/Fiche_poste.php
+++ ci306/application/views/Page/Fiche_poste.php
@@ -4,24 +4,161 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url();?>Assets/CSS/stylefdposte.css">
     <title>Fiche</title>
 </head>
 <body class="main">
     <div class="container">
 <form action="<?php echo base_url('form/form_fiche');?>" method="POST" >
-        <div class="titre">
-            <h1>Votre fiche_poste</h1>
-        </div>
-        <div class="intitule">Intitulé de poste :</div>
-        <div class="poste">Poste occupé par : </div>
-        <div class="sup">Supérieur hiérarchique :  </div>
-        <div class="fonction">Fonction :  </div>
-        <div class="intitule">Exigences requises :
-            <p>Niveau requis: </p>
-            <p>Formations et qualifications nécessaires: </p>
-            <p>Compétences nécessaires (techniques, managériales, relationnelles):</p>
-        </div>
-        <div class="moyen">Moyen(humains,matériels,financiers) :
-    </div>
+<h1>Fiche de poste</h1>
+        <table id="tableau">
+            <tr class="" >
+                <td id="logo" rowspan="2">
+                    <h2>Logo</h2>
+                </td>
+                <td>
+                <strong><center>Poste occupé par</center></strong>
+                <p>XXXXXX (nom et prénom)</p>
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                <strong><center>Fonction</center></strong>
+                <p>Le nom de la fonction</p>
+
+                </td>
+            </tr>
+            <tr class="">
+                <td rowspan="2">
+                <center> AFFECTATION</center>
+                <p>Service:</p>
+                <p>Durée hebdomadaire de travail :</p>
+                </td>
+                <td>
+                Filière/catégorie/grade
+                Filière :
+                Catégorie :
+                Cadre d'emploi :
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                Supérieur hiérarchique direct
+                </td>
+            </tr>
+            <tr class="">
+                <td colspan="2">
+                AUTONOMIE ET RESPONSABILITES
+                </td>
+               
+                
+            </tr>
+            <tr class="">
+                <td colspan="2">
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td colspan="2">
+                RELATIONS FONCTIONNELLES
+                </td>
+            </tr>
+            <tr class="">
+                <td colspan="2">
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td colspan="2">
+                MOYENS TECHNIQUES MIS A DISPOSITION
+                </td>
+            </tr>
+            <tr class="">
+                <td colspan="2">
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td colspan="2">
+                MISSIONS ET ACTIVITÉS DU POSTE
+                </td>
+               
+            </tr>
+            <tr class="">
+                <td>
+                Missions
+principales
+                </td>
+                <td>
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                Missions
+spécifiques
+                </td>
+                <td>
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                Intérêts,
+contraintes,
+difficultés du
+poste
+                </td>
+                <td>
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td colspan="2">
+                COMPÉTENCES REQUISES
+                </td>
+               
+            </tr>
+            <tr class="">
+                <td>
+                Savoirs 
+                </td>
+                <td>
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                Savoirs 
+                </td>
+                <td>
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                Savoirs 
+                </td>
+                <td>
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                Savoirs faire
+                </td>
+                <td>
+                A compléter
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                Savoirs être
+                </td>
+                <td>
+                A compléter
+                </td>
+            </tr>
+        </table>
 </body>
 </html>
