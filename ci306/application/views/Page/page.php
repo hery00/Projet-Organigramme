dc6cc6436f5ac5784b6f906cfb946d59d2e586e1
Hery00 <handrianimanana@gmail.com>
2023-03-19 21:56:46 +0300

    update base

--- ci306/application/views/Page/page.php
+++ ci306/application/views/Page/page.php
@@ -1,11 +1,11 @@
 
 <?php 
 
-        $this->load->view('Page/Header');
+        // $this->load->view('Page/Header');
 
         $this->load->view($Content);
 
-        $this->load->view('Page/Footer');
+        // $this->load->view('Page/Footer');
 
 
 ?>
